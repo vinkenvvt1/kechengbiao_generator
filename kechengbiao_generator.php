5c94cae86206f6e7e38a39bbb07b412181bd0172
vinkenvvt1 <106219464+vinkenvvt1@users.noreply.github.com>
2023-09-03 11:30:51 +0800

    Update kechengbiao_generator.php

--- kechengbiao_generator.php
+++ kechengbiao_generator.php
@@ -1,9 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="skeleton.css">
+<style>
+</style>
+</head>
+<body>
+
 <?php
 $array = array("语文","数学","英语","物理","化学","政治","历史","生物","地理","体育","计算机","班会");
 $count = count($array);
 
 echo "<table>";
-echo "<tr><th></th><td><strong>星期一</strong></td><td><strong>星期二</strong></td><td><strong>星期三</strong></td><td><strong>星期四</strong></td><td><strong>星期五</strong></td></tr>";
+echo "<tr><th></th><th>星期一</th><th>星期二</th><th>星期三</th><th>星期四</th><th>星期五</th></tr>";
 echo "<tr><th>第一节</th>";
 for ($i = 0; $i < 5; $i++) {
 	$rand  = rand(0,$count-1);
@@ -55,3 +65,6 @@ echo "</tr>";
 echo "<tr><th>晚上</th><td>自习</td></tr>";
 echo "</table>";
 ?>
+
+</body>
+</html>
